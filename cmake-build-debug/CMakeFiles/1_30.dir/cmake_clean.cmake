59a67d633314a769fd7a5c107a8c5e8b16323e1f
GHhidatang <wai2333@163.com>
2021-01-30 14:48:55 +0800

    项目初始化

---
+++ cmake-build-debug/CMakeFiles/1_30.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_30.exe"
+  "1_30.exe.manifest"
+  "1_30.pdb"
+  "CMakeFiles/1_30.dir/main.c.obj"
+  "lib1_30.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1_30.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
